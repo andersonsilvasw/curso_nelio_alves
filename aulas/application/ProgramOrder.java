59c21e41bbdd3aaa99e20430084ce8649588cfc4
andersonsilvasw <andersonsilvaedif@hotmail.com>
2022-06-12 20:32:16 -0300

    Adicionado outros pedidos

--- aulas/application/ProgramOrder.java
+++ aulas/application/ProgramOrder.java
@@ -7,9 +7,15 @@ import java.util.Date;
 
 public class ProgramOrder {
     public static void main(String[] args) {
-        Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+        Order order1 = new Order(1, new Date(), OrderStatus.PENDING_PAYMENT);
+        Order order2 = new Order(2, new Date(), OrderStatus.PROCESSING);
+        Order order3 = new Order(3, new Date(), OrderStatus.SHIPPED);
+        Order order4 = new Order(4, new Date(), OrderStatus.DELIVERED);
 
-        System.out.println(order);
+        System.out.println(order1);
+        System.out.println(order2);
+        System.out.println(order3);
+        System.out.println(order4);
 
         OrderStatus os1 = OrderStatus.DELIVERED;
         OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
