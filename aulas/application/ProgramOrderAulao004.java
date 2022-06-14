c2169123ca9b4b26eb32fd45ed6ad631a698585d
andersonsilvasw <andersonsilvaedif@hotmail.com>
2022-06-14 00:29:27 -0300

    Exercício OO e composição de objetos - Aulão #004

--- aulas/application/ProgramOrderAulao004.java
+++ aulas/application/ProgramOrderAulao004.java
@@ -54,10 +54,8 @@ public class ProgramOrderAulao004 {
 
             order.addItem(it);
         }
-
         System.out.println();
         System.out.println(order);
-
         sc.close();
     }
 }
