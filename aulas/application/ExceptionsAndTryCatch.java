9786fe03bf0b1368b33bdf2698b5208130d6aa35
andersonsilvasw <andersonsilvaedif@hotmail.com>
2022-06-17 18:12:02 -0300

    Aula de Exception e uso do try/catch

---
+++ aulas/application/ExceptionsAndTryCatch.java
@@ -0,0 +1,23 @@
+package cursonelioalves.aulas.application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ExceptionsAndTryCatch {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            String[] vect = scanner.nextLine().split(" ");
+            int position = scanner.nextInt();
+            System.out.println(vect[position]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid position");
+        } catch (InputMismatchException e) {
+            System.out.println("Input error");
+        }
+
+        System.out.println("End of program");
+    }
+}
