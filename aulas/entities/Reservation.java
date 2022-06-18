8068a1620486a9565688499361651740a42b2f67
andersonsilvasw <andersonsilvaedif@hotmail.com>
2022-06-18 17:11:40 -0300

    Exception - Good solution

--- aulas/entities/Reservation.java
+++ aulas/entities/Reservation.java
@@ -1,5 +1,7 @@
 package cursonelioalves.aulas.entities;
 
+import cursonelioalves.aulas.entities.exceptions.DomainException;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -11,10 +13,10 @@ public class Reservation {
 
     private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-    public Reservation() {
-    }
-
     public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        if (!checkOut.after(checkIn)) {
+            throw new DomainException("Check-out date must be after check-in date.");
+        }
         this.roomNumber = roomNumber;
         this.checkIn = checkIn;
         this.checkOut = checkOut;
@@ -42,20 +44,27 @@ public class Reservation {
     }
 
     public void updateDates(Date checkIn, Date checkOut) {
+        Date now = new Date();
+        if (checkIn.before(now) || checkOut.before(now)) {
+            throw new DomainException("Reservation dates for update must be future dates.");
+        }
+        if (!checkOut.after(checkIn)) {
+            throw new DomainException("Check-out date must be after check-in date.");
+        }
         this.checkIn = checkIn;
         this.checkOut = checkOut;
     }
 
-    @Override
-    public String toString() {
-        return "Room "
-                + roomNumber
-                + ", check-in: "
-                + sdf.format(checkIn)
-                + ", check-out: "
-                + sdf.format(checkOut)
-                + ", "
-                + duration()
-                + " nights";
+        @Override
+        public String toString() {
+            return "Room "
+                    + roomNumber
+                    + ", check-in: "
+                    + sdf.format(checkIn)
+                    + ", check-out: "
+                    + sdf.format(checkOut)
+                    + ", "
+                    + duration()
+                    + " nights";
     }
 }
